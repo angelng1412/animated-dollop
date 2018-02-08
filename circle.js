54d6d640af5a2a0115c73f22d0fcaee0a1237a1f
angelng1412 <ang7@stuy.edu>
2018-02-08 14:33:32 -0500

    bouncy ball

--- circle.js
+++ circle.js
@@ -2,16 +2,20 @@ var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
 context.fillStyle = "#ff0000";
 
+//buttons
+var circleButton = document.getElementById("circle");
+var saverButton = document.getElementById("screensaver");
 var stopButton = document.getElementById("stop");
-var x = 0; 
 
-var clear = function(){
+
+var clear = function(e){
+    context.beginPath(); 
     context.clearRect(0, 0, canvas.width, canvas.height);
 }
 
 
-
-var animate = function(e){
+//growing and shrinking circle
+var circle = function(e){
     var radius = 0;
     var growing = true; 
     
@@ -40,10 +44,47 @@ var animate = function(e){
     }
     animateCircle(); 
 }
+circleButton.addEventListener("click", circle); 
 
-canvas.addEventListener("click", animate); 
 
-var stopit = function(){
+//DVD player screensaver
+var DVD = function(e){
+    clear(); 
+    var x = Math.random()*canvas.width;
+    var y = Math.random()*canvas.height;
+    var xchange = 1;
+    var ychange = 1; 
+
+    var animateDVD = function(){
+	clear(); 
+	context.beginPath(); 
+	context.arc(x, y, 50, 0, 2*Math.PI);
+	context.fill();
+
+	if (x <= 0){
+	    xchange = 1;
+	}
+	else if (x >= canvas.width){
+	    xchange = -1;
+	}
+	if (y <= 0){
+	    ychange = 1;
+	}
+	else if (y >= canvas.height){
+	    ychange = -1;
+	}
+	x += xchange;
+	y += ychange; 
+	requestID = window.requestAnimationFrame(animateDVD);
+	console.log(requestID); 
+    }
+    animateDVD(); 
+}
+saverButton.addEventListener("click", DVD); 
+
+
+//stop 
+var stopit = function(e){
     window.cancelAnimationFrame(requestID);
 }
 stopButton.addEventListener("click", stopit); 
